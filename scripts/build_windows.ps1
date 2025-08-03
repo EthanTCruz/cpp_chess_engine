0e0a7686229579ff5d2fae4d8b67431d4f0d564e
EthanTCruz <90341834+EthanTCruz@users.noreply.github.com>
2025-08-02 21:36:57 -0400

    Handle Debug and Release SFML DLLs

--- scripts/build_windows.ps1
+++ scripts/build_windows.ps1
@@ -1,3 +1,11 @@
 cmake -S . -B build
 cmake --build build
-Copy-Item build/_deps/sfml-build/bin/*.dll build -Force
+
+# Copy SFML runtime libraries next to the built executable(s)
+Copy-Item build/_deps/sfml-build/bin/**/*.dll build -Recurse -Force
+if (Test-Path build/Debug) {
+    Copy-Item build/_deps/sfml-build/bin/**/*.dll build/Debug -Recurse -Force
+}
+if (Test-Path build/Release) {
+    Copy-Item build/_deps/sfml-build/bin/**/*.dll build/Release -Recurse -Force
+}
